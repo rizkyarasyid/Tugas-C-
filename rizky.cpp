1f5040e4fef3a7e2ef2e178e5e3aaad7a80f3e87
rizkyarasyid <rizkyrohimat@gmail.com>
2017-03-12 17:16:30 +0700

    Tugas C++ Rizky
    Nama : Rizky Rohimat Arrasyid
    Kelas : 16B TIF Malam

---
+++ rizky.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "hello world" << endl;
+cout << "HI Dunia" << endl;
+cout << " nama  : Rizky Rohimat. A" << endl;
+cout << " kelas : TIF 16B MALAM" << endl;
+
+return 0;
+} 
