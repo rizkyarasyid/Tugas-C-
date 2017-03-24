03cd85b73c03a185572104b7eaf6e8fbada57d56
rizkyarasyid <rizkyrohimat@gmail.com>
2017-03-24 19:29:23 +0700

    Add files via upload

---
+++ modular.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+//prototype
+  void prosesBaca ();
+  void prosesTukar ();
+  void prosesTampil ();
+
+//variable global
+  int a, b, temp;
+  
+//program utama
+   int main (){
+        prosesBaca ();
+        prosesTukar ();
+        prosesTampil ();
+    return 0;
+}
+
+//proses membaca 
+  void prosesBaca () {
+cout << " Masukkan nilai A : ";
+cin >> a;
+cout << " Masukkan nilai B : ";
+cin >> b;
+}
+
+//proses tukar
+  void prosesTukar() {
+temp = b;
+b = a;
+a = temp;
+}
+
+//proses tampil
+  void prosesTampil () {
+cout << "===================" << endl;
+cout << "Nilai A sekarang : " << a << endl;
+cout << "Nilai B sekarang : " << b;	
+}
