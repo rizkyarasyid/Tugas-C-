b415bae31c378c2a317544c9d128feefc6d37144
rizkyarasyid <rizkyrohimat@gmail.com>
2017-03-31 20:08:33 +0700

    Tugas Perbandingan angka

---
+++ Perbandingan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int perbandingan (int a, int b);
+int main () {
+
+	int nilai1, nilai2;
+	
+cout << " Masukkan nilai a : ";
+cin >> nilai1;
+cout << "Masukkan nilai b : ";
+cin >> nilai2;
+cout << " Nilai Maksimum adalah : " << perbandingan (nilai1, nilai2);
+}
+
+
+int perbandingan (int a, int b) {
+return max (a,b);
+}
